74d9b5da640c9eb4bd90e2587bbf94db031fc6b1
Jun <neohwj28@gmail.com>
2022-12-25 23:35:04 +0800

    Add hello react with JSX
    Add hello react with JSX.

--- react-first-application/src/index.js
+++ react-first-application/src/index.js
@@ -2,4 +2,20 @@ import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.querySelector("#root"));
 
-root.render(<h1>Hello React</h1>);
+const word = "React";
+const bool = true;
+
+root.render(
+  <>
+    <h1 className="title">
+      {/* JavaScript (variable, boolean, functions, etc.) */}
+      Hello {<em>there</em>} {word} {bool ? "Yes" : "No"}
+    </h1>
+    <p>
+      Lorem <strong>ipsum</strong> dolor sit amet consectetur adipisicing elit.
+      Mollitia voluptatibus pariatur, itaque omnis sunt distinctio cum nobis
+      earum quod quis labore blanditiis deserunt voluptatum, veniam modi
+      aliquid? Magnam, ea debitis!
+    </p>
+  </>
+);
