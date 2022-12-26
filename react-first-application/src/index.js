b7f9c4722f69df9282ff779f6495414f00809c17
Jun <neohwj28@gmail.com>
2022-12-26 20:03:08 +0800

    Add CSS styling
    Add CSS styling.

--- react-first-application/src/index.js
+++ react-first-application/src/index.js
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
+import "./style.css";
 
 const root = createRoot(document.querySelector("#root"));
 
@@ -7,11 +8,15 @@ const bool = true;
 
 root.render(
   <>
-    <h1 className="title">
+    {/* HTML (JSX) */}
+    <h1
+      className="title"
+      style={{ color: "coral", backgroundColor: "floralwhite" }}
+    >
       {/* JavaScript (variable, boolean, functions, etc.) */}
       Hello {<em>there</em>} {word} {bool ? "Yes" : "No"}
     </h1>
-    <p>
+    <p className="cute-paragraph">
       Lorem <strong>ipsum</strong> dolor sit amet consectetur adipisicing elit.
       Mollitia voluptatibus pariatur, itaque omnis sunt distinctio cum nobis
       earum quod quis labore blanditiis deserunt voluptatum, veniam modi
