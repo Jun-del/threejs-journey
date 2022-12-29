a42d9b0fc19f45617b0bcce362a4b874f19a03e5
Jun <neohwj28@gmail.com>
2022-12-30 01:18:32 +0800

    Fetch API for name using asynchronous function
    Fetch API for name using asynchronous function.

--- react-first-application/src/People.js
+++ react-first-application/src/People.js
@@ -1,21 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function People() {
-  const [people, setPeople] = useState([
-    { id: 1, name: "John" },
-    { id: 2, name: "Jane" },
-    { id: 3, name: "Jack" },
-    { id: 4, name: "Jill" },
-  ]);
+  const [people, setPeople] = useState([]);
 
-  const getPeople = () => {};
+  //   const getPeople = () => {
+  //     /*--- First method ---*/
+  //     // const request = window.fetch("https://jsonplaceholder.typicode.com/users");
+  //     // request.then((response) => {
+  //     //   const parse = response.json();
+  //     //   parse.then((result) => {
+  //     //     console.log(result);
+  //     //   });
+  //     // });
+
+  //     /*--- Second method ---*/
+  //     // fetch("https://jsonplaceholder.typicode.com/users")
+  //     //   .then((response) => {
+  //     //     return response.json();
+  //     //   })
+  //     //   .then((result) => {
+  //     //     console.log(result);
+  //     //   });
+
+  //     /*--- Third method ---*/
+  //     fetch("https://jsonplaceholder.typicode.com/users")
+  //       .then((response) => response.json())
+  //       .then((result) => setPeople(result));
+  //   };
+
+  const getPeople = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const result = await response.json();
+
+    setPeople(result);
+  };
+
+  // Call the function only on the first render if empty array []
+  useEffect(() => {
+    getPeople();
+  }, []);
 
   return (
     <div>
       <h2>People</h2>
       <ul>
         {people.map((person, index) => (
-          <li key={person.id}>{person.name}</li>
+          <li key={person.id}>{"Name: " + person.name}</li>
         ))}
       </ul>
     </div>
