3f94921ec971a8afee68da69fdcb81178b071fc8
Jun <neohwj28@gmail.com>
2023-01-04 14:16:42 +0800

    Add cube, sphere, and plane meshes

--- react-first-r3f-application/src/Experience.js
+++ react-first-r3f-application/src/Experience.js
@@ -2,9 +2,21 @@
 export default function Experience() {
   return (
     <>
-      <mesh>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
+      <mesh position-x={-2}>
+        {/* arguements: radius, width, height */}
+        {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
+        <sphereGeometry />
+        <meshBasicMaterial color="orange" wireframe={false} />
+      </mesh>
+
+      <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+        <boxGeometry scale={1.5} />
+        <meshBasicMaterial color="mediumpurple" wireframe={false} />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color="greenyellow" />
       </mesh>
     </>
   );
