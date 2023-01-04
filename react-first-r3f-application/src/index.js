e913a664f9ce79a510e1d2bb9c2e92bb82c5ea2c
Jun <neohwj28@gmail.com>
2023-01-04 23:09:40 +0800

    Add WebGL renderer settings

--- react-first-r3f-application/src/index.js
+++ react-first-r3f-application/src/index.js
@@ -2,11 +2,23 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.js";
+import * as THREE from "three";
+// import { CineonToneMapping } from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
-  <Canvas camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}>
+  <Canvas
+    // flat // no tone mapping
+    dpr={[1, 2]} // device pixel ratio (1 min, 2 max)
+    gl={{
+      antialias: true,
+      toneMapping: THREE.ACESFilmicToneMapping,
+      outputEncoding: THREE.sRGBEncoding,
+      alpha: true,
+    }}
+    camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}
+  >
     <Experience></Experience>
   </Canvas>
 );
